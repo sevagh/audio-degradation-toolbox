3ae9f4fae0b2e219cb1ab8264601b65b8404cb23
Sevag Hanssian <sevag.hanssian@gmail.com>
2019-03-25 18:45:42 -0700

    Casting issues

--- audio_degradation_toolbox/audio.py
+++ audio_degradation_toolbox/audio.py
@@ -1,39 +1,28 @@
 import numpy
+import array
 from pydub import AudioSegment
 from pydub.utils import get_array_type
 
 
 class Audio(object):
-    def __init__(self, path=None, ext=None, raw=None, old_faudio=None):
-        if path and (raw is not None):
+    def __init__(self, path=None, ext=None, samples=None, old_faudio=None):
+        if path and (samples is not None):
             raise ValueError("Only pass one of path[+ext] or raw[+old_faudio]")
 
         if path:
             if not ext:
                 ext = path.split(".")[-1]
             self.sound = AudioSegment.from_file(file=path, format=ext).set_channels(1)
-
-            #self.dtype = get_array_type(self.sound.sample_width * 8)
-            self.raw = numpy.frombuffer(self.sound._data, dtype=numpy.float64)
+            self.samples = self.sound.get_array_of_samples()
             self.sample_rate = self.sound.frame_rate
             self.format = ext
-        if raw is not None:
-            self.dtype = raw.dtype
-            self.raw = raw
+            self.dtype = get_array_type(self.sound.sample_width * 8)
+        if samples is not None:
+            self.samples = samples
             self.sample_rate = old_faudio.sample_rate
-            self.sound = AudioSegment(
-                self.raw,
-                frame_rate=self.sample_rate,
-                sample_width=old_faudio.sound.sample_width,
-                channels=1,
-            )
+            self.sound = old_faudio.sound._spawn(self.samples)
             self.format = old_faudio.format
+            self.dtype = old_faudio.dtype
 
     def export(self, path):
-        export_sound = AudioSegment(
-            self.raw,
-            frame_rate=self.sample_rate,
-            sample_width=self.sound.sample_width,
-            channels=1,
-        )
-        export_sound.export(out_f=path, format="wav")
+        self.sound.export(out_f=path, format="wav")
--- audio_degradation_toolbox/degradations.py
+++ audio_degradation_toolbox/degradations.py
@@ -1,21 +1,33 @@
 import numpy
 from pydub import AudioSegment
 from pydub.utils import get_array_type
+from pydub.playback import play
 from acoustics import Signal
 from acoustics.generator import noise
 import math
 from tempfile import NamedTemporaryFile
 from .audio import Audio
+import array
 
 
 def apply_noise(audio, color, snr):
-    Ps = Signal(audio.raw, audio.sample_rate).power()
-    noise_data = noise(len(audio.raw), color=color)
+    Ps = Signal(audio.samples, audio.sample_rate).power()
+    noise_data = noise(len(audio.samples), color=color)
     Pn = Signal(noise_data, audio.sample_rate).power()
     noise_data *= math.sqrt((Ps / Pn) * (10 ** (-snr / 10)))
-    noise_data = noise_data.astype(numpy.float32)
-    new_raw = audio.raw + noise_data
-    return Audio(raw=new_raw, old_faudio=audio)
+    noise_data = array.array(audio.sound.array_type, noise_data.astype(int))
+
+    # for i in range(100):
+    #    print(noise_data[i*100:(i+1)*100])
+    #    input()
+
+    a_ = Audio(samples=noise_data, old_faudio=audio)
+    play(a_.sound)
+    for i in range(len(noise_data)):
+        noise_data[i] += audio.samples[i]
+    a = Audio(samples=noise_data, old_faudio=audio)
+    play(a.sound)
+    return a_
 
 
 def mp3_transcode(audio, bitrate):
