adc2108c75097b8e57c3200af19fdcf6ebf335b0
Sevag Hanssian <sevag.hanssian@gmail.com>
2019-03-25 19:01:41 -0700

    Fixed

--- audio_degradation_toolbox/degradations.py
+++ audio_degradation_toolbox/degradations.py
@@ -2,32 +2,36 @@ import numpy
 from pydub import AudioSegment
 from pydub.utils import get_array_type
 from pydub.playback import play
-from acoustics import Signal
 from acoustics.generator import noise
 import math
 from tempfile import NamedTemporaryFile
 from .audio import Audio
 import array
+import numba
 
 
+@numba.jit
 def apply_noise(audio, color, snr):
-    Ps = Signal(audio.samples, audio.sample_rate).power()
     noise_data = noise(len(audio.samples), color=color)
-    Pn = Signal(noise_data, audio.sample_rate).power()
-    noise_data *= math.sqrt((Ps / Pn) * (10 ** (-snr / 10)))
+
+    Ps = 0
+    Pn = 0
+
+    for i in range(len(audio.samples)):
+        Ps += abs(audio.samples[i]) * abs(audio.samples[i])
+        Pn += abs(noise_data[i]) * abs(noise_data[i])
+    Ps /= len(audio.samples)
+    Pn /= len(audio.samples)
+
+    k_factor = math.sqrt((Ps / Pn) * (10 ** (-snr / 10)))
+    noise_data *= k_factor
+
     noise_data = array.array(audio.sound.array_type, noise_data.astype(int))
 
-    # for i in range(100):
-    #    print(noise_data[i*100:(i+1)*100])
-    #    input()
-
-    a_ = Audio(samples=noise_data, old_faudio=audio)
-    play(a_.sound)
     for i in range(len(noise_data)):
         noise_data[i] += audio.samples[i]
     a = Audio(samples=noise_data, old_faudio=audio)
-    play(a.sound)
-    return a_
+    return a
 
 
 def mp3_transcode(audio, bitrate):
